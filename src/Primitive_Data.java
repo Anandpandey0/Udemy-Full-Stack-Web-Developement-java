45361f72273d5e786bb302822a43ba650a556d3d
Anandpandey0 <75582022+Anandpandey0@users.noreply.github.com>
2023-08-15 14:24:25 +0530

    Initial commit

---
+++ src/Primitive_Data.java
@@ -0,0 +1,11 @@
+
+public class Primitive_Data {
+
+	public static void main(String[] args) {
+		int a = 512;
+		double percentage = a * 0.46f;
+		System.out.println("Percentage is: "+ percentage);
+
+	}
+
+}
---
+++ src/Variables.java
@@ -0,0 +1,11 @@
+
+public class Variables {
+
+	public static void main(String[] args) {
+		int a = 5;
+		String str = "Hey, this is string";
+		System.out.println(str);
+		System.out.println("a is: " + a);
+	}
+
+}
